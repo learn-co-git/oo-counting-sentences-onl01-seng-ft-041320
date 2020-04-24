220d5d197a76416b4d853206f40c0e1a4bea1152
Neil Empey <argus.two.2@gmail.com>
2020-04-24 05:37:06 +0000

    Automatically backed up by Learn

--- spec/count_sentences_spec.rb
+++ spec/count_sentences_spec.rb
@@ -43,9 +43,9 @@ describe String do
     end
 
     it "returns the number of sentences in a complex string" do
-      complex_string = 
+      complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
       expect(complex_string.count_sentences).to eq(4)
-    end"This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+    end
   end
 
 
